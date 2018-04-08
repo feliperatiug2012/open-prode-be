d88ac9e0b577e58fa86fddd43f0cf2d1027b6ffc
enrique <enrique.j.talavera@gmail.com>
2018-04-08 17:48:59 -0300

    Modificado gitignore

--- src/public/index.php
+++ src/public/index.php
@@ -38,7 +38,7 @@ $container['db'] = function ($c) {
  *************************************************************/
 $app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
     $name = $args['name'];
-    $response->getBody()->write("Hello, $name");
+    $response->getBody()->write("Hello my friends, $name");
 
     return $response;
 });
