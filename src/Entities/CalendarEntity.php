9ebaba0fdbfb35c60530caff0e52f50adb785386
nullpointer <rbenal@gmail.com>
2018-04-15 11:43:20 -0300

    Definiendo clases interna de Calendar
    *- Se utilizaron los namespace de PHP para crear clases internas de la Entity
    Calendar

--- src/Entities/CalendarEntity.php
+++ src/Entities/CalendarEntity.php
@@ -5,60 +5,71 @@
 	 * Date: 4/8/18
 	 * Time: 7:58 PM
 	 */
-	namespace OpenFixture\Entities;
+	namespace OpenFixture\Entities{
 
-	class CalendarMatch
-	{
-		public $date;
-		public $team_1;
-		public $team_2;
-		public $stadium;
+		use OpenFixture\Entities\Calendar\Team;
+		use OpenFixture\Entities\Calendar\CalendarMatch;
 
-		public function __construct($date, $team_1, $team_2, $stadium)
-		{
-			$this->date = $date;
-			$this->team_1 = $team_1;
-			$this->team_2 = $team_2;
-			$this->stadium = $stadium;
+		class CalendarEntity {
+			/**
+			 * @var  CalendarMatch[]
+			 */
+			public $phase ;
+			/**
+			 * @var Team[]
+			 */
+			public $matches;
+
+			public function __construct(array $matches)
+			{
+
+				$this->phase = $matches[0]['phase'];
+				foreach ($matches as $k => $v) {
+					//$this->phase=$v['Phase'];
+					$team1 = new Team ($v['flag_team_a'], $v['name_team_a'], $v['goals_team_a']);
+					$team2 = new Team($v['flag_team_b'], $v['name_team_b'], $v['goals_team_b']);
+					$stadium = $v['stadium'];
+
+					$cm = new CalendarMatch($v['date'], $team1, $team2, $stadium);
+
+					$this->matches[] = $cm;
+
+				}
+			}
+
+
 		}
 	}
-
-	class Team
-	{
-		public $teamFlag;
-		public $name;
-		public $goals;
-
-		public function __construct($teamFlag, $name, $goals)
+	// this is like having inner classes inside Calendar Entity
+	namespace OpenFixture\Entities\Calendar {
+		class CalendarMatch
 		{
-			$this->goals = $goals;
-			$this->name = $name;
-			$this->teamFlag = $teamFlag;
-		}
+			public $date;
+			public $team_1;
+			public $team_2;
+			public $stadium;
 
-	}
-
-	class CalendarEntity
-	{
-		public $phase;
-		public $matchs;
-
-		public function __construct(array $matchs)
-		{
-
-			$this->phase = $matchs[0]['phase'];
-			foreach ($matchs as $k => $v) {
-				//$this->phase=$v['Phase'];
-				$team1 = new Team ($v['flag_team_a'], $v['name_team_a'], $v['goals_team_a']);
-				$team2 = new Team($v['flag_team_b'], $v['name_team_b'], $v['goals_team_b']);
-				$stadium = $v['stadium'];
-
-				$cm = new CalendarMatch($v['date'], $team1, $team2, $stadium);
-
-				$this->matchs[] = $cm;
-
+			public function __construct($date, $team_1, $team_2, $stadium)
+			{
+				$this->date = $date;
+				$this->team_1 = $team_1;
+				$this->team_2 = $team_2;
+				$this->stadium = $stadium;
 			}
 		}
 
+		class Team
+		{
+			public $teamFlag;
+			public $name;
+			public $goals;
 
+			public function __construct($teamFlag, $name, $goals)
+			{
+				$this->goals = $goals;
+				$this->name = $name;
+				$this->teamFlag = $teamFlag;
+			}
+
+		}
 	}
